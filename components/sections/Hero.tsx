8ae151856a5687ad13809111eb043db7936c1a43
aarush-glitch <hgupta164@gmail.com>
2025-09-17 13:46:40 +0530

    Add LeetCode profile

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -120,6 +120,27 @@ export default function Hero() {
                   <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                 ),
               },
+              {
+                href: "https://leetcode.com/aarush_codes/", // Replace with your actual LeetCode profile URL
+                icon: (
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    strokeWidth="2" 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round"
+                  >
+                    <path d="M12 13H19.5" />
+                    <path d="M9.424 7.268l4.999 -4.999" />
+                    <path d="M16.633 16.644l-2.402 2.415a3.189 3.189 0 0 1 -4.524 0l-3.77 -3.787a3.223 3.223 0 0 1 0 -4.544l3.77 -3.787a3.189 3.189 0 0 1 4.524 0l2.302 2.313" />
+                  </svg>
+                ),
+              },
+
             ].map((link, idx) => (
               <a
                 key={idx}
